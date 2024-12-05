d56f5fcf43f49c19e8316b689c7e70ad73a23fdb
DaniilKrasnenkov81 <krasnenkovdaniil1@gmail.com>
2024-12-05 10:02:03 +0300

    Update task2.c
    Красненков Даниил Евгеньевич

--- 3824Б1ПР1/13 Красненков Даниил Евгеньевич/task2.c
+++ 3824Б1ПР1/13 Красненков Даниил Евгеньевич/task2.c
@@ -1,6 +1,11 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
+  unsigned long long c = unsigned long long(a) * unsigned long long(b);
+  if (c>=UNIT_MAX){
+    return UNIT_MAX;
+  }
   return 0;
 }
